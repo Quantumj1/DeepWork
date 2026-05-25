03d7024f132820132306657b4d76863be56bac57
Quantumj1 <126617989+Quantumj1@users.noreply.github.com>
2026-05-25 15:37:36 +0000

    Update Home.jsx

--- deepwork/src/components/Home.jsx
+++ deepwork/src/components/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
   return (
       <div className="max-w-6xl text-center w-full min-h-screen flex flex-col items-center justify-center px-4">
         <div className="mb-2 p-4 content-center w-full">
-          <img className='mx-auto w-20 h-20 rounded-full' src="../image/high-resolution-color-logo.png" alt="DeepWork Logo" />
+          <img className='mx-auto w-20 h-20 rounded-full' src="./image/high-resolution-color-logo.png" alt="DeepWork Logo" />
           <h1 className='text-5xl md:text-7xl text-transparent mb-10 bg-clip-text text-white'>Welcome To DeepWork</h1>
           <p className='text-5xl md:text-2xl max-w-3xl mx-auto bg-clip-text text-white'>Productivity Reimagined</p>
           <p className="text-5xl md:text-2xl max-w-3xl mx-auto bg-clip-text text-white">
